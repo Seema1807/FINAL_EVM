a71e23535485bde4bdfffdaa5634bbe413d08226
seema <naikseema205@gmail.com>
2017-06-08 17:53:47 -0700

    3rd commit

---
+++ src/BallotUITest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BallotUITest {
+
+	
+	@Test
+	public void test() {
+		BallotUI b1=new BallotUI();
+		//fail("Not yet implemented");
+	}
+
+}
